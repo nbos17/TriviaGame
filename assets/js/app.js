05e9a358dbda93c23d24581f305efa22eb071283
Your Name <nbos0531@gmail.com>
2017-12-22 23:43:38 -0500

    final3

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,7 +144,7 @@ function generateImage (object) {
 }
 
 function timer() {
-  var t = 11;
+  var t = 10;
   var countdowns = setInterval(countdown, 1000);
     function countdown() {
       if (t === 0) {
